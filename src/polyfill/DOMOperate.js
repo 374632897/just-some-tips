a7e95f4b4e21a9081d17f93e8eaa5f3c7a685a00
374632897 <374632897@qq.com>
2016-02-27 10:46:27 +0800

    comite

---
+++ src/polyfill/DOMOperate.js
@@ -0,0 +1,12 @@
+/*
+* @Author: liujiandong
+* @Date:   2016-02-27 10:38:27
+* @Last Modified by:   liujiandong
+* @Last Modified time: 2016-02-27 10:39:45
+*/
+
+'use strict';
+Object.prototype.addClass = function (cls) {
+  // if ()
+  this.className += ' ' + cls;
+}
