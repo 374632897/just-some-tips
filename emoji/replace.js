56ff64312040a2aa7e60e836b43c7b938ffd8e5e
374632897 <374632897@qq.com>
2016-06-27 09:23:56 +0800

    :smirk:

--- emoji/replace.js
+++ emoji/replace.js
@@ -2,19 +2,15 @@
 * @Author: Jiang Guoxi
 * @Date:   2016-06-24 13:18:15
 * @Last Modified by:   Jiang Guoxi
-* @Last Modified time: 2016-06-24 14:31:44
+* @Last Modified time: 2016-06-27 09:23:19
 */
 
 const fs = require('fs');
-const height = 881 * 64;
+const RATIO = 64 / 25; // 64 => 雪碧图ICON宽高 25 => 期待宽高
 fs.readFile('./sprite-2.scss', (err, data) => {
   const str = data.toString();
   const _str = str.replace(/background-position: 0 (-?\d+(?:px)?);/g, (match, position) => {
-    // console.log(match, position);
-    // return 'background-position: 0 ' + (- parseInt(position, 10) / height * 100).toFixed(6) + '%;';
-    return 'background-position: 0 ' + (parseInt(position, 10) / 2.56) + 'px;';
+    return 'background-position: 0 ' + (parseInt(position, 10) / RATIO) + 'px;';
   });
-  fs.writeFileSync('./replace.scss', _str)
-  // const _t = fs.createWriteStream('./replace.scss');
-  // _t.pipe(_str);
+  fs.writeFileSync('./replace.scss', _str);
 });
