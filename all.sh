8b610b5eaf3aed2b09358dfe2ddae55a3299e748
374632897 <374632897@qq.com>
2016-07-01 18:34:27 +0800

    :smirk:

---
+++ all.sh
@@ -0,0 +1,9 @@
+# @Author: Jiang Guoxi
+# @Date:   2016-07-01 18:34:12
+# @Last Modified by:   Jiang Guoxi
+# @Last Modified time: 2016-07-01 18:34:17
+
+#! /usr/bin/ bash
+git add .
+git commit -m ':smirk:'
+git push
