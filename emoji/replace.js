ded0ee9141401328d1c6c5a79044296819ae525e
374632897 <374632897@qq.com>
2016-07-26 14:39:16 +0800

    :smirk:

--- emoji/replace.js
+++ emoji/replace.js
@@ -2,15 +2,17 @@
 * @Author: Jiang Guoxi
 * @Date:   2016-06-24 13:18:15
 * @Last Modified by:   Jiang Guoxi
-* @Last Modified time: 2016-06-27 09:23:19
+* @Last Modified time: 2016-07-26 14:10:42
 */
 
 const fs = require('fs');
 const RATIO = 64 / 25; // 64 => 雪碧图ICON宽高 25 => 期待宽高
-fs.readFile('./sprite-2.scss', (err, data) => {
+fs.readFile('./square-t.css', (err, data) => {
   const str = data.toString();
-  const _str = str.replace(/background-position: 0 (-?\d+(?:px)?);/g, (match, position) => {
-    return 'background-position: 0 ' + (parseInt(position, 10) / RATIO) + 'px;';
+  // const _str = str.replace(/width: 64px;\r\n/g, '').replace(/height: 64px;\r\n/g, '');
+  // console.log(_str);
+  const _str = str.replace(/background-position: (-?\d+(?:px)?) (-?\d+(?:px)?);/g, (match, a, b) => {
+    return 'background-position: ' + (parseInt(a, 10) / RATIO) + 'px ' + (parseInt(b, 10) / RATIO) + 'px;';
   });
-  fs.writeFileSync('./replace.scss', _str);
+  fs.writeFileSync('./square-t-2.css', _str);
 });
