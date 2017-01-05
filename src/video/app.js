d36a38fb601ebbd30b5c2f7eb78228507030d5e2
Jason <374632897@qq.com>
2017-01-05 15:34:52 +0800

    :smirk:

--- src/video/app.js
+++ src/video/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 const bodyParser = require('body-parser');
-
+const { getVideo, getList } = require('./template');
 app.use(bodyParser.json());
+// app.use('/dist', express.static('dist'));
+
+app.use('/dist', express.static(path.join(__dirname, 'dist'))); // 需要用这个才行。
+
 app.get('/', (req, res) => {
   console.log(req.headers);
   res.send('Hello world');
@@ -10,4 +15,8 @@ app.get('/', (req, res) => {
 app.get('/secret', (req, res) => {
   res.send('Secret');
 });
+app.get(/.+\.mp4$/, (req, res) => {
+  // console.log(req.url.slice(1))
+  res.send(getVideo('http://www.w3school.com.cn/i/movie.mp4'));
+})
 app.listen(3000);
--- src/video/template.js
+++ src/video/template.js
@@ -1,2 +1,39 @@
 const fs = require('fs');
 const path = require('path');
+
+function getVideo (addr) {
+  return `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>${addr}</title>
+  <link href="/dist/video-js.min.css" rel="stylesheet">
+  <script src="/dist/video.min.js"></script>
+</head>
+<body>
+<video  preload = "auto" id = "my-player" class = 'video-js' controls data-setup = "{}" >
+  <source src="${addr}" type="video/mp4" >
+</video>
+</body>
+</html>
+  `;
+}
+function getList (list) {
+  const html = list.reduce((prev, next) => {
+    return prev + `<li><a href = "${next}">${path.basename(next).split('.')[0]}</a><li>`
+  }, '');
+  return `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>首页</title>
+</head>
+<body>
+  <ul>${html}</ul>
+</body>
+</html>
+  `;
+}
+
+exports.getVideo = getVideo;
+exports.getList = getList;
