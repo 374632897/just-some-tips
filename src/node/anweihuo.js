6d0ceba890680cf8c46cdca54cf8ccc447cd6fd3
Jason <374632897@qq.com>
2017-01-19 08:42:34 +0800

    添加判断对象是否相等的函数

--- src/node/anweihuo.js
+++ src/node/anweihuo.js
@@ -1,8 +1,22 @@
-const { random } = require('../../utils/array');
-for (let i = 0; i < 100000; i++) {
-  const item = Math.random() * i, parseItem = parseInt(item, 10), hItem = item | 0;
-  if (parseItem !== hItem) {
-    console.info('notEqual');
-    consoleinfo('item => %s, parseItem => %s, hItem => %s', item, parseItem, hItem);
-  }
+const { random, range } = require('../../utils/array');
+const { tip, doLoop, isObjectEqual } = require('../../utils/func');
+const arr = range(0, 10000).map(item => Math.random() * item);
+
+function loop () {
+  doLoop(() => {
+    const item = Math.random() * i, parseItem = parseInt(item, 10), hItem = item | 0;
+      if (parseItem !== hItem) {
+        console.info('notEqual');
+        consoleinfo('item => %s, parseItem => %s, hItem => %s', item, parseItem, hItem);
+      }
+  }, 100000);
 }
+let parse, parse2;
+tip(function parseItem () {
+  parse = arr.map(item => parseInt(item, 10));
+});
+tip(function hItem () {
+  parse2 = arr.map(item => item | 0);
+});
+
+console.log(isObjectEqual(parse, parse2));
--- utils/func.js
+++ utils/func.js
@@ -1,5 +1,6 @@
 exports.tip = tip;
 exports.doLoop = doLoop;
+exports.isObjectEqual = isObjectEqual;
 
 function tip (fn = function () {}, tips = fn.name) {
   console.time(tips);
@@ -11,5 +12,12 @@ const MAX = 10000;
 function doLoop (fn = function () {}, max = MAX) {
   for (let i = 0; i < max; i++) {
     fn();
+  }
+}
+function isObjectEqual (source = {}, dist = {}) {
+  try {
+    return JSON.stringify(source) === JSON.stringify(dist);
+  } catch (e) {
+    return false;
   }
 }
