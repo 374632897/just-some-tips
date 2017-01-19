38d8327c87abb86d9d146c3d03904d64ea9d2ac5
Jason <374632897@qq.com>
2017-01-19 08:53:54 +0800

    添加条件判断函数 & 测试用例

--- tests/func.js
+++ tests/func.js
@@ -19,7 +19,48 @@ d('# func', () => {
       assert.equal(obj.a, 90);
     });
   });
+  d('# doLoopWithCondition', () => {
+    const doLoopWithCondition = func.doLoopWithCondition;
+    it('should be breaked when the calculate value is false', () => {
+      let i = 0;
+      doLoopWithCondition(() => {
+        if (++i === 100) return false;
+      }, 100000);
+      assert.equal(i, 100);
+    });
+  });
   d('# tip', () => {
     it.skip('Should console the time of function execute');
+  });
+  d('# isObjectEqual', () => {
+    const isObjectEqual = func.isObjectEqual;
+    it('should be equal when two array has same item and order', () => {
+      const a = [1,2,3,4,5], b = [1,2,3,4,5];
+      assert.equal(isObjectEqual(a, b), true);
+    });
+    it('should be not equal when two array has same item but different order', () => {
+      const a = [1,2,3,4,5], b = [1,2,3,5,4];
+      assert.equal(isObjectEqual(a, b), false);
+    });
+    it('it should be true when the object has same key-value pair', () => {
+      const a = {
+        name: 'Jason',
+        age: 23
+      }, b = {
+        name: 'Jason',
+        age: 23
+      };
+      assert.equal(isObjectEqual(a, b), true);
+    });
+    it('it should be false when the object has same key-value pair', () => {
+      const a = {
+        name: 'Jason',
+        age: 23
+      }, b = {
+        name: 'Jason',
+        age: 24
+      };
+      assert.equal(isObjectEqual(a, b), false);
+    });
   });
 });
--- utils/func.js
+++ utils/func.js
@@ -1,5 +1,6 @@
 exports.tip = tip;
 exports.doLoop = doLoop;
+exports.doLoopWithCondition = doLoopWithCondition;
 exports.isObjectEqual = isObjectEqual;
 
 function tip (fn = function () {}, tips = fn.name) {
@@ -14,7 +15,13 @@ function doLoop (fn = function () {}, max = MAX) {
     fn();
   }
 }
+function doLoopWithCondition (fn = function () {}, max = MAX) {
+  for (let i = 0; i < max; i++) {
+    if (fn() === false) break;
+  }
+}
 function isObjectEqual (source = {}, dist = {}) {
+  if (source === dist) return true;
   try {
     return JSON.stringify(source) === JSON.stringify(dist);
   } catch (e) {
