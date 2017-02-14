14dbdae0e250b38c419f19b130427b880d6ffa5b
Jason <374632897@qq.com>
2017-02-14 21:19:26 +0800

    :smirk:

--- src/weibo/readUser.js
+++ src/weibo/readUser.js
@@ -1,15 +1,29 @@
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('./follower.log'));
-const followers = [];
-data.forEach(item => {
-  item.forEach(_item => {
-    const [ids, names] = _item.split('&');
-    followers.push({
-      id: ids.split('=')[1],
-      name: names.split('=')[1].match(/(\S+?)\\/g)[0].slice(0, -1)
+
+exports.getData = getData;
+exports.getIds = getIds;
+
+function getData (data) {
+  const followers = new Set();
+  data.forEach(item => {
+    item.forEach(_item => {
+      const [ids, names] = _item.split('&');
+      followers.push({
+        id: ids.split('=')[1],
+        name: names.split('=')[1].match(/(\S+?)\\/g)[0].slice(0, -1)
+      });
     });
   });
-});
-const ids = [...new Set(followers.map(item => item.id))];
+  return followers;
+}
+
+function getIds (followers) {
+  return [...new Set(followers.map(item => item.id))];
+}
+
+const followers = getData(data);
+const ids = getIds(followers);
+
 console.log(ids);
 console.log(ids.length)
