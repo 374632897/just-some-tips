de4fbbab287a2c06bd577c96ac26541956df6f29
Jason <374632897@qq.com>
2017-05-06 22:21:36 +0800

    update

--- test/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ $1 ]; then
-  echo "The first argument is $1";
-else
-  echo "There is not a argument provided";
-fi;
--- test/testdir/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-count=0;
-while (( count < 10)); do
-  echo "test haha" >> testfile${count};
-  (( count++ ));
-done;
-echo -e "\033[35mdone\033[0m";
-
