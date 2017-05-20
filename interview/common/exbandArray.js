fb18b9e5bfa16febae6b861d2baa787f2fe49dba
Jason <374632897@qq.com>
2017-05-20 15:05:54 +0800

    update name

--- interview/common/exbandArray.js
+++
@@ -1,9 +0,0 @@
-// 数组扁平化
-function expandArray (ary) {
-  if (ary.length <= 1) return ary;
-  return ary.reduce((curr, next) => {
-    return curr.concat(next.length > 1 ? expandArray(next) : next);
-  }, []);
-}
-
-console.log(expandArray([1,2,[3,4,5,[6,7,8], 9]]))
